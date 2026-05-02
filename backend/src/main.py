5f370760a825c9ad882d396434f98894e562a363
Marie Marie <est.maria.ocampo@unimilitar.edu.co>
2026-05-02 10:35:35 -0500

    main

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,13 +5,25 @@ from src.historias.router import router as historia_router
 
 app = FastAPI(title="Clínica Multimedia Salud S.A. API", version="1.0.0")
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://clinica-grupoN.lab.umng.edu.co"],
+    allow_origins=[
+        "http://localhost:4200",
+        "http://127.0.0.1:4200"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# app.add_middleware(
+#    CORSMiddleware,
+#   allow_origins=["https://clinica-grupoN.lab.umng.edu.co"],
+#   allow_credentials=True,
+#   allow_methods=["*"],
+#   allow_headers=["*"],
+#
 
 app.include_router(auth_router, prefix="/api/auth", tags=["auth"])
 app.include_router(historia_router, prefix="/api", tags=["historias"])
