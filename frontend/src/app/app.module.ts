e4d19026a94b370945a8dc13a9253833da9b1168
Gabriela Espinosa <est.mariag.espinosa@unimilitar.edu.co>
2026-05-26 17:30:47 -0500

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,17 +11,19 @@ import { MfaComponent } from './auth/mfa/mfa.component';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 import { AuthGuard } from './shared/guards/auth.guard';
 import { OrdenMedicaComponent } from './medico/orden-medica/orden-medica.component';
+import { ReporteAuditoriaComponent } from './admin/reporte-auditoria/reporte-auditoria.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'mfa', component: MfaComponent },           // <-- AÑADE ESTA RUTA
   { path: 'historia/:cedula', component: HistoriaComponent, canActivate: [AuthGuard] },
   { path: 'orden-medica', component: OrdenMedicaComponent, canActivate: [AuthGuard] },
+  { path: 'reporte-auditoria', component: ReporteAuditoriaComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/login' }
 ];
 
 @NgModule({
-  declarations: [AppComponent, HistoriaComponent, LoginComponent, MfaComponent, OrdenMedicaComponent], // Añade MfaComponent aquí también
+  declarations: [AppComponent, HistoriaComponent, LoginComponent, MfaComponent, OrdenMedicaComponent, ReporteAuditoriaComponent], 
   imports: [
     BrowserModule,
     HttpClientModule,
